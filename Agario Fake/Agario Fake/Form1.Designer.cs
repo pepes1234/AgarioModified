610062beeb9b0c6e3cf849daa2bbdfd9166183aa
pepes1234 <lucas.pepes07@gmail.com>
2022-09-16 20:45:35 -0300

    Implementação completa do projeto

---
+++ Agario Fake/Agario Fake/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Agario_Fake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(361, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Massa:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 479);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Agario Fake/Agario Fake/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agario_Fake
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Massa = new List<PictureBox>();
+        List<PictureBox> BolasDivididas = new List<PictureBox>();
+        Random rnd = new Random();
+
+        int x, y;
+        int playerSpeed = 8;
+        int spawnTime = 20;
+
+        Color[] newColor = {Color.Red, Color.Green, Color.Blue, Color.Gold, Color.Turquoise };
+
+        bool goUp, goDown, goLeft, goRight, spaceConfirm, shiftConfirm;
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == Convert.ToChar(Keys.Space))
+            {
+                spaceConfirm = true;
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SplitPicturebox()
+        {
+            pictureBox1.Width = pictureBox1.Width / 2;
+            pictureBox1.Height = pictureBox1.Height / 2;
+            PictureBox picbox = new PictureBox();
+            picbox.Width = pictureBox1.Width;
+            picbox.Height = pictureBox1.Height;
+            picbox.BackColor = pictureBox1.BackColor;
+
+            BolasDivididas.Add(picbox);
+            picbox.Location = new Point((pictureBox1.Location.X + pictureBox1.Width) - pictureBox1.Width / 8, (pictureBox1.Location.Y + pictureBox1.Height) - pictureBox1.Height / 8);
+            BolasDivididas.Add(picbox);
+            this.Controls.Add(picbox);
+        }
+        private void MakePictureBox()
+        {
+            PictureBox pictureboxaleatoria = new PictureBox();
+            pictureboxaleatoria.Height = 30;
+            pictureboxaleatoria.Width = 30;
+            pictureboxaleatoria.BackColor = newColor[rnd.Next(0, newColor.Length)];
+
+            x = rnd.Next(10, this.ClientSize.Width - pictureboxaleatoria.Width);
+            y = rnd.Next(10, this.ClientSize.Height - pictureboxaleatoria.Height);
+
+            pictureboxaleatoria.Location = new Point(x, y);
+
+            Massa.Add(pictureboxaleatoria);
+            this.Controls.Add(pictureboxaleatoria);
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x = 0;
+            if (goLeft)
+            {
+                pictureBox1.Left -= playerSpeed;    
+                foreach(PictureBox bolas in BolasDivididas)
+                {
+                   
+                }
+            }
+            if (goRight)
+            {
+                pictureBox1.Left += playerSpeed;
+            }
+            if (goUp)
+            {
+                pictureBox1.Top -= playerSpeed;
+            }
+            if (goDown)
+            {
+                pictureBox1.Top += playerSpeed;
+            }
+
+                
+            if(pictureBox1.Width == 15)
+            {
+                label1.Text = "Massa: " + pictureBox1.Width / 15;
+            }
+            else
+            {
+                label1.Text = "Massa: " + ((pictureBox1.Width - 15) / 2).ToString();
+            }
+            
+            
+
+            spawnTime -= 1;
+
+            
+
+            if (spawnTime < 1)
+            {
+                MakePictureBox(); 
+                spawnTime = 20;
+            }
+            foreach (PictureBox massa in Massa.ToList())
+            {
+                if (pictureBox1.Bounds.IntersectsWith(massa.Bounds))
+                {
+                    pictureBox1.Width += 20;
+                    pictureBox1.Height += 20;
+                    Massa.Remove(massa);
+                    this.Controls.Remove(massa);
+                }
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up  || e.KeyCode == Keys.W)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Shift)
+            {
+                shiftConfirm = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+            if(e.KeyCode == Keys.Shift)
+            {
+                shiftConfirm = false;
+            }
+        }
+    }
+}
